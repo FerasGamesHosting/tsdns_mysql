26b22b9d36ebebc0b508845ea4a48841aa4be12a
Rafael <rafael_andrade74@hotmail.com>
2020-09-25 18:05:30 -0300

    Aplicacao final

---
+++ api.js
@@ -0,0 +1,93 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+var config = require('./config.json');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host     : config.host,
+  user     : config.user,
+  password : config.password,
+  database : config.database
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+db.query("CREATE TABLE IF NOT EXISTS zones (id integer primary key, zone varchar(100),target varchar(50))");
+
+
+app.get('/list', function (req, res) {
+  if( req.headers.authorization == config.api_key ){
+    var zone = req.params.zone;
+    db.query("SELECT * FROM zones", function(err, rows) {
+      res.send('{"result":"success","message":' + JSON.stringify( rows ) + '}');
+      console.log('Consultando base completa :o, tu ta doido!');
+    });
+  }else{
+    res.statusCode = 403;
+    res.send('{"result":"error","message":"Invalid auth token"}');
+  }
+});
+
+app.get('/add/:zone/:target', function (req, res) {
+  if( req.headers.authorization == config.api_key ){
+    var zone = req.params.zone;
+    var target = req.params.target;
+    db.query("SELECT * FROM zones WHERE zone=?",zone, function(err, row) {
+    if(row != '')
+    {
+      console.log("caiu no existe!");
+    }else
+    {
+      var sql = "INSERT INTO zones(zone,target) VALUES(?,?)";
+      db.query(sql,[zone,target]);
+    }
+      res.statusCode = 201;
+      res.send('{"result":"success","message":' + JSON.stringify( row ) + '}');
+      console.log('Zona: ' + zone + ' existe!');
+    });
+      
+  }else{
+    res.statusCode = 403;
+    res.send('{"result":"error","message":"Invalid auth token"}');
+  }
+});
+
+app.get('/del/:zone', function (req, res) {
+  if( req.headers.authorization == config.api_key ){
+    var zone = req.params.zone;
+    var sql = "DELETE FROM zones WHERE zone =?";
+    db.query(sql,zone, function(err, row) {
+      res.statusCode = 202;
+      res.send('{"result":"success","message":' + JSON.stringify( row ) + '}');
+      console.log('Zona: ' + zone + ' deletada com sucesso!');
+    });
+   
+  }else{
+    res.statusCode = 403;
+    res.send('{"result":"error","message":"Invalid auth token"}');
+  }
+});
+
+app.get('/get/:zone', function (req, res) {
+  if( req.headers.authorization == config.api_key ){
+    var zone = req.params.zone;
+    db.query("SELECT * FROM zones WHERE zone=?",zone, function(err, row) {
+      res.statusCode = 200;
+      res.send('{"result":"success","message":' + JSON.stringify( row ) + '}');
+      console.log('Zona: ' + zone + ' consultada com sucesso!');
+    });
+  }else{
+    res.statusCode = 403;
+    res.send('{"result":"error","message":"Invalid auth token"}');
+  }
+});
+
+module.exports = app;
---
+++ tsdns.js
@@ -0,0 +1,64 @@
+"use strict";
+var net = require('net');
+const express = require('express');
+const mysql = require('mysql');
+var config = require('./config.json');
+
+const db = mysql.createConnection({
+    host     : config.host,
+    user     : config.user,
+    password : config.password,
+    database : config.database
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+//const app = express();
+
+var sockets = [];
+var tsdns = net.createServer(function (socket) {
+        sockets.push(socket);
+        var writeEnd = function(message) {
+            socket.write(message, function() {
+                socket.end();
+            });
+        };
+        var freeTimeout = setTimeout(function() {
+           writeEnd('404');
+        }, 60000);
+        socket.on('data', function(data) {
+            var domain = data.toString().replace(/\r|\n/g, '');
+            db.query("SELECT * FROM zones WHERE zone=?",domain, function(err, rows){
+                if( err ){
+                    console.log(err);
+                }else{
+                    console.log(rows);
+                    if( rows.length ){
+                       writeEnd(rows[0].target);
+                    }else{
+                       writeEnd('404');
+                    }
+               }
+            });
+           
+        });
+        socket.on('close', function() {
+            for (var i in sockets) {
+                if (sockets[i] === socket) {
+                    sockets.splice(i, 1);
+                }
+            }
+        })
+        socket.on('error', function(error) {});
+    });
+    tsdns.on('close', function() {
+        db.close();
+    })
+
+module.exports = tsdns;
